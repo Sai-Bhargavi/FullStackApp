cf63eb2cb734b4bef06d67a49ad4c232853eb4f7
Sai Bhargavi Aireni <asaibhargavi@gmail.com>
2022-07-12 09:25:36 -0400

    working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import './App.css';
 import axios from "axios";
+import { TableCell, TableRow } from "@mui/material";
 
 class App extends React.Component<any, any> {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      category: '',
-      status: ''
+      plants: []
     }
     this.handleSub = this.handleSub.bind(this);
-    this.handleOnClick = this.handleOnClick.bind(this);
+    this.getTableData = this.getTableData.bind(this);
   }
 
   handleSub(e) {
@@ -33,19 +32,33 @@ class App extends React.Component<any, any> {
       .catch(err => console.log(err));
   }
 
-  handleOnClick() {
-    console.log("sai");
+  componentDidMount() {
+    //axios.get('/plant').then(res => console.log(res.data[1].name));
+    axios.get('/plant').then(response => this.setState({ plants: response.data })).catch((error) => {
+      console.log(error);
+    });
   }
 
-  componentDidMount() {
-    axios.get('/plant').then(res => console.log(res.data[1].name));
+  getTableData() {
+    let b = JSON.stringify(this.state.plants);
+    if (!b) return null;  //added this line
+
+    return Object.keys(b).map((row, index) => (
+      <TableRow key={index} >
+        <TableCell>row</TableCell>
+        <TableCell>row</TableCell>
+      </TableRow>
+    ))
   }
 
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
+          <ul>{this.state.plants.map(plant => <li> {JSON.stringify(plant)} </li>)}</ul>
 
+          {/* {<button onClick={this.getTableData}>Get Data</button>} */}
           <form onSubmit={this.handleSub}>
             <label>Name</label>
             <input type="text" name="name" />
